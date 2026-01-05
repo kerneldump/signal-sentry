7267e0dcf50d9365b313183bbecde9e416371a3e
AM <kerneldump@gmail.com>
2026-01-04 20:33:41 -0800

    test(config): Add failing test for validateInterval

---
+++ config_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestValidateInterval(t *testing.T) {
+	tests := []struct {
+		input    int
+		wantErr  bool
+	}{
+		{5, false},   // Default/Valid
+		{1, false},   // Valid
+		{100, false}, // Valid
+		{0, true},    // Invalid (Zero)
+		{-1, true},   // Invalid (Negative)
+	}
+
+	for _, tt := range tests {
+		err := validateInterval(tt.input)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("validateInterval(%d) error = %v, wantErr %v", tt.input, err, tt.wantErr)
+		}
+	}
+}
