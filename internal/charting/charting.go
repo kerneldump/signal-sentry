b7d7ddce230228d51af01bf809c6f245267b3f71
AM <kerneldump@gmail.com>
2026-01-05 18:35:11 -0800

    feat: enhance chart with stddev and time axis

--- internal/charting/charting.go
+++ internal/charting/charting.go
@@ -5,7 +5,6 @@ import (
 	"image/color"
 	"image/png"
 	"os"
-	"time"
 
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
@@ -24,17 +23,24 @@ func Generate(data []models.CombinedStats, outputFile string) error {
 
 	// Prepare plotters
 	latencyXYs := make(plotter.XYs, len(data))
+	stdDevXYs := make(plotter.XYs, len(data))
 	rsrpXYs := make(plotter.XYs, len(data))
 	sinrXYs := make(plotter.XYs, len(data))
 	lossXYs := make(plotter.XYs, len(data))
 
-	startTime := time.Unix(data[0].Gateway.Time.LocalTime, 0)
+	// Helper to safely cast time
+	getTime := func(t int64) float64 {
+		return float64(t)
+	}
 
 	for i, d := range data {
-		t := time.Unix(d.Gateway.Time.LocalTime, 0).Sub(startTime).Minutes()
+		t := getTime(d.Gateway.Time.LocalTime)
 
 		latencyXYs[i].X = t
-		latencyXYs[i].Y = d.Ping.Avg // Using Avg for the main line
+		latencyXYs[i].Y = d.Ping.Avg
+
+		stdDevXYs[i].X = t
+		stdDevXYs[i].Y = d.Ping.StdDev
 
 		rsrpXYs[i].X = t
 		rsrpXYs[i].Y = float64(d.Gateway.Signal.FiveG.RSRP)
@@ -46,20 +52,33 @@ func Generate(data []models.CombinedStats, outputFile string) error {
 		lossXYs[i].Y = d.Ping.Loss
 	}
 
+	// Common Time Ticker
+	// We use the standard TimeTicks which interprets float64 X values as unix seconds
+	timeTicks := plot.TimeTicks{Format: "15:04"}
+
 	// Create Plots
 	pLat := plot.New()
-	pLat.Title.Text = "Latency (AVG)"
+	pLat.Title.Text = "Latency & StdDev"
 	pLat.Y.Label.Text = "ms"
-	pLat.X.Label.Text = "Time (min)"
+	pLat.X.Tick.Marker = timeTicks
+
 	lineLat, _ := plotter.NewLine(latencyXYs)
 	lineLat.Color = color.RGBA{R: 0, G: 0, B: 255, A: 255} // Blue
-	pLat.Add(lineLat)
+
+	lineStd, _ := plotter.NewLine(stdDevXYs)
+	lineStd.Color = color.RGBA{R: 0, G: 255, B: 255, A: 255}           // Cyan
+	lineStd.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)} // Dashed
+
+	pLat.Add(lineLat, lineStd)
+	pLat.Legend.Add("Avg", lineLat)
+	pLat.Legend.Add("StdDev", lineStd)
 	pLat.Add(plotter.NewGrid())
 
 	pH := plot.New()
 	pH.Title.Text = "Signal Strength"
 	pH.Y.Label.Text = "dBm / dB"
-	pH.X.Label.Text = "Time (min)"
+	pH.X.Tick.Marker = timeTicks
+
 	lineRSRP, _ := plotter.NewLine(rsrpXYs)
 	lineRSRP.Color = color.RGBA{R: 255, G: 0, B: 0, A: 255} // Red
 
@@ -74,33 +93,24 @@ func Generate(data []models.CombinedStats, outputFile string) error {
 	pLoss := plot.New()
 	pLoss.Title.Text = "Packet Loss"
 	pLoss.Y.Label.Text = "%"
-	pLoss.X.Label.Text = "Time (min)"
+	pLoss.X.Tick.Marker = timeTicks
 	pLoss.Y.Min = 0
 	pLoss.Y.Max = 100
+
 	lineLoss, _ := plotter.NewLine(lossXYs)
 	lineLoss.Color = color.RGBA{R: 0, G: 0, B: 0, A: 255} // Black
-
-	// Create a filled polygon for loss to make it stand out
-	// We'd need to construct the full polygon loop, but line is fine for now for MVP.
 
 	pLoss.Add(lineLoss)
 	pLoss.Add(plotter.NewGrid())
 
-	// Combine into a single image using a Tiles arrangement or just create 3 separate plots drawn to one canvas
-	// Gonum doesn't have a built-in "subplot" layout system as simple as Matplotlib,
-	// so we create a large canvas and draw each plot onto sub-tiles.
-
+	// Combine into a single image
 	const width = 10 * vg.Inch
 	const height = 12 * vg.Inch
 
-	// Create a canvas with white background
 	c := vgimg.NewWith(vgimg.UseWH(width, height), vgimg.UseBackgroundColor(color.White))
 	dc := draw.New(c)
 
 	// Layout: 3 rows, 1 column
-	// Top: Latency
-	// Mid: Signal
-	// Bot: Loss
 
 	rectLat := draw.Canvas{
 		Canvas: dc,
